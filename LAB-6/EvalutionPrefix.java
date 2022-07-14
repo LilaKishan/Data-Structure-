788a0cbbc2c921535fdad03d1058b054e7f8c330
Kishan Lila <94374447+LilaKishan@users.noreply.github.com>
2022-07-14 21:11:25 +0530

    Add files via upload

---
+++ LAB-6/EvalutionPrefix.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class EvalutionPrefix {
+    static Stack<Integer> stack = new Stack<Integer>();
+
+    public static void stackPush(int x){
+        stack.push(x);
+    }
+
+    public static void operate(int x){
+        //ascii value '-' =45,'+'=43,'*'=42,'/'=47
+        if(x==45){
+            int operand1=stack.pop();
+            int operand2=stack.pop();
+            int Rvalue=operand1 - operand2;
+            stack.push(Rvalue);
+        }
+        else if(x==43){
+            int operand1=stack.pop();
+            int operand2=stack.pop();
+            int Rvalue=operand1 + operand2;
+            stack.push(Rvalue);
+        }
+        else if(x==42){
+            int operand1=stack.pop();
+            int operand2=stack.pop();
+            int Rvalue=operand1 * operand2;
+            stack.push(Rvalue);
+        }
+        else{
+            int operand1=stack.pop();
+            int operand2=stack.pop();
+            int Rvalue=operand1 /operand2;
+            stack.push(Rvalue);
+        }
+        
+    }
+
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String st=sc.next();
+    // stack.push(0);
+    for(int i=st.length()-1;i>=0;i--){
+        int x=st.charAt(i);
+        if(x==45 ||x==42||x==43||x==47){
+            operate(x);
+        }
+        else{
+             int temp=(int)x-48;
+            stackPush(temp);
+        }
+    }
+    System.out.println(stack.pop());
+    }
+}
